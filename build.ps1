d0d52212e1488f11685cfdffe9f79de480a81c20
Mirage <mirageyangkang@outlook.com>
2018-03-26 17:39:16 +0800

    Window构建工程完善

--- build.ps1
+++ build.ps1
@@ -11,7 +11,8 @@ if( -not (Test-Path MERender_Build_Windows) )
 }
 Set-Location ./MERender_Build_Windows
 
-cmake -G "Visual Studio 15 2017 Win64" ../MirageRender
+#cmake -G "Visual Studio 15 2017 Win64" ../MirageRender
+cmake -G "Visual Studio 14 2015 Win64" ../MirageRender
 
 Write-Host '****************************************'
 Write-Host 'MERender build over'
